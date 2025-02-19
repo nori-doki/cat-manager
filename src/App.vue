58956422869e928adeac5d403f77dbd5a76b514d
ines-dai <inesdai59@gmail.com>
2025-02-19 19:02:19 +0100

    feat: cat list display, cat frame ui

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import CatCategories from './components/organisms/CatCategories.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Les Minous Oubliés</h1>
+    <CatCategories />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import './reset.css'
+import './style.scss'
 import App from './App.vue'
+import 'primeicons/primeicons.css'
 
 createApp(App).mount('#app')
